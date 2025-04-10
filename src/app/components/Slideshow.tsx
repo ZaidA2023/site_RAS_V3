d4193d35e087e9e68693f0f5e569d03477644d78
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2025-04-10 15:40:55 -0500

    it doesn't work

--- src/app/components/Slideshow.tsx
+++ src/app/components/Slideshow.tsx
@@ -27,16 +27,14 @@ const Slideshow = ({ slides }) => {
           {slides.map((slide, idx) => (
             <SplideSlide key={idx}>
               <div className="relative">
-                <div className="w-full h-[200px] bg-gray-200 flex items-center justify-center">
                   <Image
                     src={slide.img}
                     alt={`Slide ${slide.id}`}
                     width={300}
                     height={200}
-                    className="w-full object-cover cursor-pointer slide-img background-grey" // background grey so when image doesn't load it shows up like this
+                    className="w-full object-cover cursor-pointer slide-img"
                     onClick={() => handleImageClick(slide.link)}
                   />
-                </div>
                 {slide.caption && (
                   <div className="text-left text-black font-bold text-2xl pl-2 mt-2">
                     {slide.caption}
