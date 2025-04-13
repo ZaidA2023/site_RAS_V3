6a53646453f4b66fb7e8cc0fbd10217731c302e0
Azaid <zaida2023@outlook.com>
2025-04-13 17:13:28 -0500

    im sory commit count

--- src/app/embed/page.js
+++ src/app/embed/page.js
@@ -1,23 +1,32 @@
-'use client'
+// /app/embed/page.js
+"use client";
 
-import { useSearchParams } from 'next/navigation';
+export const dynamic = "force-dynamic";
+
+import { useSearchParams } from "next/navigation";
 import Navbar from "../components/Navbar";
 import React, { Suspense } from "react";
-//import Footer from '../components/footer';
 
 export default function Embed() {
   const searchParams = useSearchParams();
-  const source = searchParams.get('source');
-  
+  const source = searchParams.get("source");
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <div>
-      <div><Navbar scrollSet={false} opacitySet={false} inverse={true}/></div>
-      <iframe 
-        src={source}
-        style={{width: "100%", height: "200vh", border: "none", padding: "0", position: "absolute", marginTop:-35}}
-      />
-    </div>
+      <div>
+        <Navbar scrollSet={false} opacitySet={false} inverse={true} />
+        <iframe
+          src={source}
+          style={{
+            width: "100%",
+            height: "200vh",
+            border: "none",
+            padding: "0",
+            position: "absolute",
+            marginTop: -35,
+          }}
+        />
+      </div>
     </Suspense>
   );
 }
